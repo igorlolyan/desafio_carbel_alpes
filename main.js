e5860c0f0a73c6ca70fef303f952084d34e66449
igorlolyan <igor.lol.yan@gmail.com>
2024-02-21 11:49:47 -0300

    Atualizando projeto

---
+++ main.js
@@ -0,0 +1,40 @@
+$(".botaoToggle").click(function () {
+    $("#seletorSection").slideToggle();
+});
+
+$(".formToggle").click(function () {
+    $("#formDiv").slideToggle();
+});
+
+
+$(".multiple-items").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+});
